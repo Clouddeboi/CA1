addc2d406595a176f9baed6a69c73ff27d6ce53f
Cloud <michalcloud2003@gmail.com>
2023-10-01 03:26:16 +0100

    Made a tutorial like area to test implemented features + platforms
    ^^yeah thats pretty much it
    i ajusted the bounce force of the jumpad and moved around a bunch of platfroms.

--- 2d_Game_CA1/Assets/Scripts/Platforms Scripts/JumpPad.cs
+++ 2d_Game_CA1/Assets/Scripts/Platforms Scripts/JumpPad.cs
@@ -7,7 +7,7 @@ public class JumpPad : MonoBehaviour
     //Tutorial followed from: https://www.youtube.com/watch?v=0e3Ld6-RzIU
 
     //jumpad variables
-    private float bounce = 20f;
+    private float bounce = 30f;
 
     private void OnCollisionEnter2D(Collision2D collision)//check if something collided with object
     {
